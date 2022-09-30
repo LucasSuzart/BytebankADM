cf15a62b19755e7b6f73cf368f3dc047235cfb8a
Perkles <lucasbatistasuzart@gmail.com>
2022-09-30 12:59:10 -0300

    comeco do projeto

---
+++ Funcionarios/Funcionario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ADM.Funcionarios
+{
+    public class Funcionario
+    {
+        public string Nome;
+        public string cpf;
+        public double salario;
+
+        public double getBonificacao()
+        {
+            return salario * 0.1;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+using bytebank_ADM.Funcionarios;
+
+Console.WriteLine("Hello, World!");
+
+Funcionario Pedro = new Funcionario();
+
+Pedro.Nome = "João";
+Pedro.cpf = "1234678";
+Pedro.salario = 2000;
+
+Console.WriteLine("Bonificação: " + Pedro.getBonificacao());
